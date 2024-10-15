8991b4d82a17c1340ad53ad6035d03cb2fe810fc
wingled22 <pelayowindel@gmail.com>
2024-10-15 15:18:29 +0800

    ..

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -3,40 +3,24 @@ import 'package:flutter/material.dart';
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
+  
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: GridView.count(
-          crossAxisCount: 4,
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(3.0),
-              child: Container(color: Colors.blue,),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(3.0),
-              child: Container(color: Colors.blue,),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(3.0),
-              child: Container(color: Colors.blue,),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(3.0),
-              child: Container(color: Colors.blue,),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(3.0),
-              child: Container(color: Colors.blue,),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(3.0),
-              child: Container(color: Colors.blue,),
-            ),
-            
-          ],
-        )
+        body: GridView.builder(
+          itemCount: 10,
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 4), 
+          itemBuilder: (context, index){
+            return Padding(
+              padding: const EdgeInsets.all(3),
+              child: Container(
+                color: Colors.blue,
+                child: Text(index.toString()),
+              ),
+            );
+          })
       ),
     );
   }
