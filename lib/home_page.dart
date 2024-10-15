f92be67ac56b01a9f42304fdd1be35fa886dd0b0
wingled22 <pelayowindel@gmail.com>
2024-10-15 14:47:10 +0800

    moved the homepage class

---
+++ lib/home_page.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: ListView(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                height: 200, 
+                decoration: const BoxDecoration(
+                  color: Colors.deepPurpleAccent
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                height: 200, 
+                decoration: const BoxDecoration(
+                  color: Colors.deepPurpleAccent
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                height: 200, 
+                decoration: const BoxDecoration(
+                  color: Colors.deepPurpleAccent
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                height: 200, 
+                decoration: const BoxDecoration(
+                  color: Colors.deepPurpleAccent
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                height: 200, 
+                decoration: const BoxDecoration(
+                  color: Colors.deepPurpleAccent
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                height: 200, 
+                decoration: const BoxDecoration(
+                  color: Colors.deepPurpleAccent
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
