af39071cd6f22309c3fe05d3a56f74bb5ef557ac
wingled22 <pelayowindel@gmail.com>
2024-10-15 14:46:00 +0800

    simple list view

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:oct1424/home_page.dart';
 
 void main() {
   runApp(const HomePage());
-}
-
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
-  }
 }
