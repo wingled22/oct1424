9535cd0874d491c098020b329cdbc457291d03a0
wingled22 <pelayowindel@gmail.com>
2024-10-15 14:57:27 +0800

    updated

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -5,6 +5,26 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Placeholder();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: ListView.builder(
+          itemCount: 10,
+          itemBuilder: (context , index){
+            return Padding(
+              padding: const EdgeInsets.all(5.0),
+              child: Container(
+                height: 70,
+                color: Colors.deepPurple,
+                child:  Text(
+                  index.toString(),
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            );
+          }
+          ),
+      ),
+    );
   }
 }
